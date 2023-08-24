e9908073962b18dbcbd53cc0fed14bdf38de3e9f
Inkver <inkver@yandex.ru>
2023-08-24 14:16:12 +0300

    Add DZ #2

---
+++ task11/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98 */
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int numberOne = number / 100;
+Console.WriteLine(numberOne);
+
+int numberTwo = number % 10;
+Console.WriteLine(numberTwo);
+
+string strNumberOne = Convert.ToString(numberOne);
+string strNumberTwo = Convert.ToString(numberTwo);
+
+string numberFinish = strNumberOne + strNumberTwo;
+int intnumberFinish = Convert.ToInt32(numberFinish);
+Console.WriteLine(intnumberFinish);
