a068549f5cbd61f3a0c5f4584b52869037f9122a
Inkver <inkver@yandex.ru>
2023-09-01 15:54:24 +0300

    Add HM6

---
+++ task41(HW6)/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Write("Введите, какое количество чисел необходимо поситать: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+int[] FillUserArray(int num)
+{   
+    Console.Write("Введите по очереди заданное количество чисел: ");
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array [i] = Convert.ToInt32(Console.ReadLine());
+      
+    }
+    return array;
+}
+
+void PrintArray(int[] arrayToPrint)
+{   
+    int posCount = 0;
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {   
+        if (arrayToPrint[i] > 0)
+        {
+            posCount += 1;
+        }
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+        Console.Write (", ");
+        }
+    }
+    Console.Write($"] > количество цифр больше 0: {posCount}");
+}
+
+int[] conclusion = FillUserArray(numbers);
+PrintArray(conclusion);
---
+++ task42/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+string Division(int a)
+{
+    string result = string.Empty;
+    int temp = 0;
+    for (int i = a; i > 0; i/=2)
+    {
+        temp = i % 2;
+        result = temp + result;
+    }
+return result;
+}
+
+string conclusion = Division(a);
+Console.WriteLine(conclusion);
