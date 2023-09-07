c17e6ee8ebb42075443eb908745812a4b932741b
Inkver <inkver@yandex.ru>
2023-09-07 12:47:47 +0300

    Add Home Work 7

---
+++ task47(HW7)/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+double[,] FillArray(double[,] matrix, int from, int before)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(from, before) + new Random().NextDouble();
+        }
+    }
+    return matrix;
+}
+
+
+
+double[,] matr = new double[4,4];
+FillArray(matr, 1, 10);
+PrintArray(matr);
---
+++ task52(HW7)/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.Clear();
+
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int[,] matrix, int from, int before)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(from, before);
+        }
+    }
+    return matrix;
+}
+
+void ArithmAvr(int[,] matrix)
+{   Console.Write($"Среднее арифметическое каждого столбца: ");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {   
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+            count += 1;
+        }
+        double arAv = Convert.ToDouble(sum) / Convert.ToDouble(count);
+        Console.Write($"{arAv} ");
+    }
+}
+
+int[,] matr = new int[4,4];
+FillArray(matr, 1, 10);
+PrintArray(matr);
+ArithmAvr(matr);
